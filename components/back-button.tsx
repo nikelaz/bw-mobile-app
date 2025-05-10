83896f4566ab2acc1dcefc2ca6f22ddc0a953708
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-05-10 15:51:02 +0300

    refactor: improve BackButton component

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,31 +1,49 @@
 import { Platform, TouchableOpacity, StyleSheet, GestureResponderEvent } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useThemeColor } from '@/hooks/useThemeColor';
+import { useMemo } from 'react';
 
 const styles = StyleSheet.create({
   wrapper: {
-    paddingRight: 20
-  }
+    paddingRight: 20,
+  },
 });
 
 export interface BackButtonProps {
   onPress: ((event: GestureResponderEvent) => void);
-  label?: string;
+  accessibilityLabel?: string;
 }
 
-const BackButton = (props: any) => {
-  const isIOS = Platform.OS === 'ios';
-  const backIcon = isIOS ? 'chevron-back-outline' : 'arrow-back-outline'
+const BackButton = ({
+  onPress,
+  accessibilityLabel = 'go back',
+}: BackButtonProps) => {
   const primaryColor = useThemeColor({}, 'primary');
   const textColor = useThemeColor({}, 'text');
-  const color = isIOS ? primaryColor : textColor;
-  const iconSize = isIOS ? 28 : 25;
+
+  const buttonProps = useMemo(() => {
+    const isIOS = Platform.OS === 'ios';
+    return {
+      backIcon: isIOS ? 'chevron-back-outline' : 'arrow-back-outline',
+      color: isIOS ? primaryColor : textColor,
+      iconSize: isIOS ? 28 : 25,
+    };
+  }, [primaryColor, textColor]);
 
   return (
-    <TouchableOpacity onPress={props.onPress} style={styles.wrapper}>
-      <Ionicons size={iconSize} name={backIcon} color={color} />
+    <TouchableOpacity 
+      onPress={onPress} 
+      style={styles.wrapper}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityRole="button"
+    >
+      <Ionicons 
+        size={buttonProps.iconSize} 
+        name={buttonProps.backIcon} 
+        color={buttonProps.color} 
+      />
     </TouchableOpacity>
   );
-}
+};
 
 export default BackButton;
