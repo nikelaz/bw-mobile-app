3a4370c411babe66da62fc878bb8dc165d7879f3
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-05-11 10:00:50 +0300

    refactor: Heading component

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,26 +1,36 @@
-import { ThemedText } from './themed-text';
 import { StyleSheet } from 'react-native';
+import { ThemedText } from './themed-text';
 
 type HeadingProps = Readonly<{
   children: React.ReactNode,
   level?: 1 | 2,
 }>;
 
-const Heading = (props: HeadingProps) => (
-  <ThemedText style={{
-    ...styles.heading,
-    fontSize: props.level && props.level === 2 ? 22 : 28,
-  }}>
-    {props.children}
-  </ThemedText>
-);
+const Heading = (props: HeadingProps) => {
+  const headingStyle = props.level === 2 ? styles.heading2 : styles.heading1;
+  
+  return (
+    <ThemedText style={headingStyle}>
+      {props.children}
+    </ThemedText>
+  );
+};
 
 const styles = StyleSheet.create({
-  heading: {
+  heading1: {
     fontSize: 28,
-    lineHeight: 30,
+    lineHeight: 34, 
+    fontWeight: 600,
+  },
+  heading2: {
+    fontSize: 22,
+    lineHeight: 28,
     fontWeight: 600,
   },
 });
+
+Heading.defaultProps = {
+  level: 1,
+};
 
 export default Heading;
